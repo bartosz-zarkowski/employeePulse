1cd93bd2b15a0f6b78dcd6beb3d80740d7c28193
arte00 <hubi.lukki@gmail.com>
2022-05-20 04:55:37 +0200

    final pipeline test

--- gs-spring-boot-main/src/test/java/com/example/springboot/HelloControllerIT.java
+++ gs-spring-boot-main/src/test/java/com/example/springboot/HelloControllerIT.java
@@ -1,5 +1,8 @@
 package com.example.springboot;
 
+import com.example.springboot.controllers.LoginController;
+import com.example.springboot.controllers.SurveyController;
+import com.example.springboot.services.SurveyService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,14 +13,20 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import java.util.Base64;
 
 
-
+@SpringBootTest
 public class HelloControllerIT {
 
 	@Autowired
-	private TestRestTemplate template;
+	private SurveyController surveyController;
+
+	@Autowired
+	private LoginController loginController;
+
+	@Autowired
+	private SurveyService surveyService;
 
 	@Test
-	public void getHello() throws Exception {
+	public void testEncryption() throws Exception {
 		String password = "password123";
 		String passwordEncoded = Base64.getEncoder().encodeToString(password.getBytes());
 
@@ -26,5 +35,24 @@ public class HelloControllerIT {
 
 		Assertions.assertEquals(password, decodedPassword);
 	}
+
+	@Test
+	public void testLoginController() throws Exception {
+		Assertions.assertNotNull(loginController);
+	}
+
+	@Test
+	public void testSurveyController() throws Exception {
+		Assertions.assertNotNull(surveyController);
+	}
+
+	@Test
+	public void testDateConverter() throws Exception {
+		String date = "2022-05-19T18:46";
+		String convertedDate = surveyService.dateToLocalDate(date);
+		Assertions.assertEquals("2022-05-19", convertedDate);
+	}
+
+
 
 }
